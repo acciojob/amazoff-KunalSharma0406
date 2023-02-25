04e0cdcc8309db732c10b782f5302d1d84c48aa6
KunalSharma0406 <118346621+KunalSharma0406@users.noreply.github.com>
2023-02-25 20:08:01 +0530

    Solution 3 contest ass

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,40 +1,23 @@
 package com.driver;
 
 public class Order {
-    private String orderId;
-    private String deliveryTime;
-    private String partnerId;
 
-    public Order(String orderId, String deliveryTime, String partnerId) {
-        this.orderId = orderId;
-        this.deliveryTime = deliveryTime;
-        this.partnerId = partnerId;
+    private String id;
+    private int deliveryTime;
+
+    public Order(String id, String deliveryTime) {
+
+        // The deliveryTime has to converted from string to int and then stored in the attribute
+        //deliveryTime  = HH*60 + MM
     }
 
-    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public int getDeliveryTime() {return deliveryTime;}
 
     public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getDeliveryTime() {
-        return deliveryTime;
-    }
-
-    public void setDeliveryTime(String deliveryTime) {
-        this.deliveryTime = deliveryTime;
-    }
-
-    public String getPartnerId() {
-        return partnerId;
-    }
-
-    public void setPartnerId(String partnerId) {
-        this.partnerId = partnerId;
+        return null;
     }
 }
-
